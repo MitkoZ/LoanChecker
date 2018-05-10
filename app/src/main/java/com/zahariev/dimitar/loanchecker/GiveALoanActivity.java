c58ff3153d0eefa8643846bf4ea6f16de917a0fe
MitkoZ <mitkoz15@abv.bg>
2018-05-10 14:31:46 +0300

    Add functionality - GiveALoanActivity currency is updated from database

--- app/src/main/java/com/zahariev/dimitar/loanchecker/GiveALoanActivity.java
+++ app/src/main/java/com/zahariev/dimitar/loanchecker/GiveALoanActivity.java
@@ -2,19 +2,30 @@ package com.zahariev.dimitar.loanchecker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.zahariev.dimitar.bindmodels.LoanBindModel;
 import com.zahariev.dimitar.utils.Utils;
 
+import java.util.ArrayList;
+import java.util.Currency;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class GiveALoanActivity extends AppCompatActivity {
 
@@ -30,12 +41,39 @@ public class GiveALoanActivity extends AppCompatActivity {
         DatePicker returnDateDatePicker = findViewById(R.id.returnDateDatePicker);
         long currentTime = new Date().getTime();
         returnDateDatePicker.setMinDate(currentTime);
+        final DatabaseReference database;
+        database = FirebaseDatabase.getInstance().getReference();
+        Query userPossibleCurrenciesQuery = database.child("userCurrencies").orderByChild("userId").equalTo(Utils.googleAccount.getId());
+        userPossibleCurrenciesQuery.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                List<String> possibleCurrenciesDropdown = parseCurrencyData(dataSnapshot);
+                SpinnerAdapter spinnerAdapter= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, possibleCurrenciesDropdown);
+                Spinner dropdown = findViewById(R.id.currencySpinner);
+                dropdown.setAdapter(spinnerAdapter);
+            }
 
-        Spinner dropdown = findViewById(R.id.currencySpinner);
-        String[] items = new String[]{"1", "2", "3"};//todo get the actual data from the database
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-        dropdown.setAdapter(adapter);
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("onCurrenciesCancelled", "loadPossibleCurrencies:onCancelled", databaseError.toException());
+            }
+        });
+
     }
+
+
+    private List<String> parseCurrencyData(DataSnapshot dataSnapshot) {
+        List<String> possibleCurrenciesDropdown = new ArrayList<>();
+        for (DataSnapshot currencyObjectFromDb : dataSnapshot.getChildren()) {
+            for (DataSnapshot currencyObjectChildren : currencyObjectFromDb.getChildren()) {
+                if (currencyObjectChildren.getKey().equals("currency")) {
+                    possibleCurrenciesDropdown.add(currencyObjectChildren.getValue().toString());
+                }
+            }
+        }
+        return possibleCurrenciesDropdown;
+    }
+
 
     public void submitLoan(View view) {
         EditText loanerNameEditText = findViewById(R.id.loanerName);
